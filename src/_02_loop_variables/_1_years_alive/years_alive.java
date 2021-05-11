4209245d27e2e7b6c018d23e4893ce9f19ce03c1
root <root@9b842724b7e0>
2021-05-11 01:26:13 +0000

    day 50 of coding

--- src/_02_loop_variables/_1_years_alive/years_alive.java
+++ src/_02_loop_variables/_1_years_alive/years_alive.java
@@ -2,18 +2,38 @@ package _02_loop_variables._1_years_alive;
 
 public class years_alive {
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	public static void main(String[] args) {
+		
+		int years=2008;
+		
+		int currentyears=2021;
+		
+		for(years=2008; years <= currentyears; years+=1) {
+			
+			System.out.println(years);
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
 }
--- src/_02_loop_variables/_3_robot_walk_of_fame/WalkOfFame.java
+++ src/_02_loop_variables/_3_robot_walk_of_fame/WalkOfFame.java
@@ -7,20 +7,35 @@ public class WalkOfFame {
 	public static void main(String[] args) {
 		Robot rob = new Robot();
 		
+		int x=30;
+		
 		// 1. Set the X position of the robot so that it starts on the left. 
 		// You also need to show the robot to see the result of this line.
-
+		rob.setX(25);
 		// 2. Make the robot draw a star shape. Hint: angle=144.
-
+		rob.penDown();
+		
+		for (int i=0;i<10;i++) {
+		
+		for (int i2=0;i2<5;i2++) {
+			
+		rob.setSpeed(999999999);
+		rob.turn(144);
 		// 3. Set the length of each line in the star to 30.
-
+		rob.move(30);
+		
 		/** THE CHALLENGE: **/
 		/*
 		 * Make the robot draw a line of 10 stars (see recipe to see how it should look)
 		 * 
 		 * Hint: The distance between stars is 50.
 		 */
-
+		}
+		x+=75;
+		rob.moveTo(x, 300);
+		
+		
+		}
 	}
 
 }
